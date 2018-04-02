4041994c3322e883b04f1cca0f4d1adb2bbaee0d
Anthony LOUP <anthony.loup@insa-lyon.fr>
2018-04-02 17:30:30 +0200

    Adding options in arguments

--- main.cpp
+++ main.cpp
@@ -9,15 +9,28 @@ using namespace std;
 using namespace antlr4;
 
 int main(int argc, char **argv){
-    ifstream prgFile;
-    prgFile.open(argv[1]);
-    string prg;
-    string line;
-    if(!prgFile.is_open()){
-        cerr << "Can't open file" << endl;
+    bool printToken = false;
+    bool printStruct = false;
+    bool printIR = false;
+    if(argc <= 1){
+        cerr << "Not enough parameters" << endl;
         return 1;
     }
+    ifstream prgFile;
+    prgFile.open(argv[1]);
+    if(!prgFile.is_open()){
+        cerr << "Can't open file" << endl;
+        return 2;
+    }
+    for(int i=2;i<argc;i++){
+        cout << "argv:" <<argv[i] << endl;
+        if((string)argv[i] == "--token") printToken = true;
+        else if((string)argv[i] == "--struct") printStruct= true;
+        else if((string)argv[i] == "--ir") printIR= true;
+    }
 
+    string prg;
+    string line;
     while (!prgFile.eof())
     {
         getline(prgFile, line);
@@ -27,15 +40,28 @@ int main(int argc, char **argv){
 	ProgLexer lexer(&input);
 	CommonTokenStream tokens(&lexer);
     tokens.fill();
-//    for (auto token : tokens.getTokens()) {
-//        std::cout << token->toString() << std::endl;
-//    }
+
 	ProgParser parser(&tokens);
 	tree::ParseTree* tree = parser.program();
 
 	Prog visitor;
 
     Program* prog = visitor.visit(tree);
+
+    if(printToken){
+        for (auto token : tokens.getTokens()) {
+            std::cout << token->toString() << std::endl;
+        }
+    }
+
+    if(printStruct){
+        prog->print();
+    }
+
+    if(printIR){
+        //todo
+    }
+
 //    on crée l'IR
     prog->buildIR();
 //    on génère l'assembleur
