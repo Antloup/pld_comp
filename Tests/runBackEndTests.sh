e557f0f69db0d78d83a7105c249a9cf9f8ba766e
Askrenteam <renaud.meurisse@yahoo.fr>
2018-04-05 10:59:27 +0200

    function return test

--- Tests/runBackEndTests.sh
+++ Tests/runBackEndTests.sh
@@ -1,10 +1,10 @@
 let passed_tests=0;
 let total_tests=0;
-for file in `find BackEndTests/IfTest -name *.cpto`; do
+for file in `find BackEndTests -name *.cpto`; do
     total_tests=$((total_tests+1));
     ../cmake-build-debug/pld_comp $file;
     file_base=`echo $file | sed "s/\.cpto//g"`;
-    gcc -c $file_base.s -o temp.o && gcc temp.o -o temp && ./temp > $file_base.output;
+    gcc -c $file_base.s -o temp.o && gcc temp.o -o temp -no-pie && ./temp > $file_base.output;
     result=`diff -q $file_base.output $file_base`;
     if [[ $result ]]; then
         echo "/!\ Test $file_base raté. :("
